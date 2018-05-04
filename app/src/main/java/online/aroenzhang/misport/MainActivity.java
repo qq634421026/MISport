6d03a45cf281a28d32a912595de2e3fcbd30876e
张洪宇 <aroenzhang@sina.com>
2018-05-04 09:56:51 +0800

    test

--- app/src/main/java/online/aroenzhang/misport/MainActivity.java
+++ app/src/main/java/online/aroenzhang/misport/MainActivity.java
@@ -1,7 +1,7 @@
 package online.aroenzhang.misport;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * @author aroenzhang
@@ -13,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    //线程池
+    //ExecutorService singleThreadPool = new ThreadPoolExecutor(1, 1, 0, TimeUnit.MILLISECONDS,
+    //    new LinkedBlockingQueue<Runnable>(1024), new ThreadPoolExecutor.AbortPolicy());
+    //singleThreadPool.execute(new Runnable() {
+    //  @Override public void run() {
+    //    Thread.currentThread().getName();
+    //  }
+    //});
+    //singleThreadPool.shutdown();
   }
 }
